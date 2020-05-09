77c1dd3182831c3b5a658484d0b46349b5c8749a
Andrew Lamoureux <andrew@vector35.com>
2020-05-08 21:44:27 -0400

    Add parameterized CRC compute method

--- crcsolver/__init__.py
+++ crcsolver/__init__.py
@@ -2,5 +2,8 @@ name = "crcsolver"
 
 from . import main
 
-def solve(data, unknowns, desired, crcfunc):
-	return main.solve(data, unknowns, desired, crcfunc)
+def solve(data, unknowns, desired, crc_func):
+	return main.solve(data, unknowns, desired, crc_func)
+
+def compute(data, crc_name):
+	return main.compute(data, crc_name)
--- crcsolver/main.py
+++ crcsolver/main.py
@@ -5,6 +5,7 @@ import binascii
 import functools
 
 from . import subsetxor
+from . import crc_catalog
 
 def solve(data, unknowns, desired, crcfunc):
 	zeroed = [0]*len(data)
@@ -47,3 +48,39 @@ def solve(data, unknowns, desired, crcfunc):
 
 	return bytes(result)
 
+def reflect(x, width):
+	y = 0
+	for i in range(width):
+		y <<= 1
+		y |= (x & 1)
+		x >>= 1
+	return y
+
+def compute(data, crc_name):
+	entry = [e for e in crc_catalog.database if e['name']==crc_name][0]
+	if not entry:
+		raise Exception('unrecognized algorithm: %s' % crc_name)
+
+	poly = entry['poly']
+	checksum = entry['init']
+	width = entry['width']
+	msb_mask = 1<<(width-1)
+
+	for b in data:
+		if entry['refin']:
+			b = reflect(b, 8)
+
+		checksum = checksum ^ (b << (width-8))
+
+		for k in range(8):
+			if checksum & msb_mask:
+				checksum = ((checksum ^ msb_mask) << 1) ^ poly
+			else:
+				checksum = checksum << 1
+
+	checksum ^= entry['xorout']
+
+	if entry['refout']:
+		checksum = reflect(checksum, 64)
+
+	return checksum
